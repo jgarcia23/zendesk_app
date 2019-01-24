1af83ed04fbc0dfb56890f7c16170006437ca1ce
joseluisgarcia <joseluisg1791@gmail.com>
2019-01-23 23:02:22 -0600

    Update

--- quick_clean/assets/main.js
+++ quick_clean/assets/main.js
@@ -1,4 +1,19 @@
 $(function() {
   var client = ZAFClient.init();
   client.invoke('resize', { width: '100%', height: '150px' });
+  showInfo();
 });
+
+function showInfo() {
+  var requester_data = {
+    'name': 'Jane Doe',
+    'tags': ['tag1', 'tag2'],
+    'created_at': 'November 20, 2014',
+    'last_login_at': 'June 27, 2016'
+  };
+
+  var source = $("#requester-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(requester_data);
+  $("#content").html(html);
+}
